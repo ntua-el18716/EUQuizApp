b3a8105a1c8e935ce72c3f402ca562b715060f1c
Stylianos <stylianoschar7@gmail.com>
2024-03-02 10:36:31 +0200

    vercel serverless api test

--- EUQuizAppFrontend/src/services/getQuizData.js
+++ EUQuizAppFrontend/src/services/getQuizData.js
@@ -1,5 +1,8 @@
 export async function getQuizData() {
-  const res = await fetch("http://localhost:3000/getQuizData");
+  // const res = await fetch("http://localhost:3000/getQuizData");
+  const res = await fetch(
+    "https://eu-quiz-app-backend-git-master-stylianos-projects.vercel.app/getQuizData",
+  );
   const quizData = await res.json();
   return quizData.dataArray;
 }
