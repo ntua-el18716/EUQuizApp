27fc777f66d0b7f8c8c90ab8915c4ec3e8d02f6f
Stylianos <stylianoschar7@gmail.com>
2024-03-11 08:11:32 +0200

    heroku api

--- EUQuizAppFrontend/src/services/apiConfig.js
+++ EUQuizAppFrontend/src/services/apiConfig.js
@@ -1,5 +1,6 @@
 // apiConfig.js
 // const apiBaseUrl = "https://eu-quiz-app-backend.vercel.app"; // Update this path according to your API structure
-const apiBaseUrl = "http://localhost:3000"; // Update this path according to your API structure
+// const apiBaseUrl = "http://localhost:3000"; // Localhost
+const apiBaseUrl = "https://cyvoteeu-ab55554a145c.herokuapp.com"; // Localhost
 
 export default apiBaseUrl;
